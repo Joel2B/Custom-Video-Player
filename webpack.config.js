a7deb3ffcc22841ecfe1cb0d168ee5163b5ad2c3
Joel2B <jbuenouas@gmail.com>
2021-06-30 22:01:15 -0600

    so that the routes are relative

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const getDistOptions = (mode) => {
         case 'development':
             return {
                 path: path.resolve(__dirname, 'dist'),
-                publicPath: '/'
+                publicPath: ''
             };
         case 'current':
             return {
