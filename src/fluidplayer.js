d7b48778381d24a9105c0f237b8ce73aec6f5c8b
Joel2B <jbuenouas@gmail.com>
2021-07-01 20:52:27 -0600

    Set fillToContainer to true

--- src/fluidplayer.js
+++ src/fluidplayer.js
@@ -161,7 +161,7 @@ const fluidPlayerClass = function () {
                 playButtonShowing: true,
                 playPauseAnimation: false,
                 closeButtonCaption: 'Close', // Remove?
-                fillToContainer: false,
+                fillToContainer: true,
                 autoPlay: false,
                 preload: 'auto',
                 mute: false,
